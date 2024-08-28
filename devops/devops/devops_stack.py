6dbb421606f05beb765237e6bd51d20bb7fbc5cc
Noel Melendez <noelmelendez@gmail.com>
2024-08-27 22:53:23 -0500

    UPDATE
    - added env to context

--- devops/devops/devops_stack.py
+++ devops/devops/devops_stack.py
@@ -1,3 +1,4 @@
+import logging
 from aws_cdk import (
     Stack,
     aws_dynamodb as dynamodb,
@@ -8,34 +9,46 @@ from aws_cdk import (
 )
 from constructs import Construct
 
+
 class DevopsStack(Stack):
     # The code that defines your stack goes here
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        table_name = "pantry"
+        env_name = self.node.try_get_context("ENV_NAME")
 
-        pantry_table = dynamodb.Table(self, id=table_name, table_name=table_name,
+        table_name = f"{env_name}-pantry"
+
+        logging.info("Environment: %s", env_name)
+
+        pantry_table = dynamodb.Table(
+            self,
+            id=table_name,
+            table_name=table_name,
             partition_key=dynamodb.Attribute(
-                name="id",
-                type=dynamodb.AttributeType.STRING
+                name="id", type=dynamodb.AttributeType.STRING
             ),
-            removal_policy=RemovalPolicy.DESTROY
+            removal_policy=RemovalPolicy.DESTROY,
         )
 
-        search_nutrition_product_lambda_function_name = "search-nutrition-product-function"
+        search_nutrition_product_lambda_function_name = (
+            "search-nutrition-product-function"
+        )
 
-        search_nutrition_products_lambda_function = _lambda.Function(self, search_nutrition_product_lambda_function_name,
         search_nutrition_products_lambda_function = _lambda.Function(
+            self,
+            search_nutrition_product_lambda_function_name,
             function_name=search_nutrition_product_lambda_function_name,
             runtime=_lambda.Runtime.NODEJS_20_X,
             handler="index.handler",
             code=_lambda.Code.from_asset("../src/lambda/searchNutritionProduct/dist"),
             description="Search operation for nutrition product",
+            environment={"PRODUCTS_TABLE_NAME": pantry_table.table_name},
         )
 
         pantry_table.grant_read_write_data(search_nutrition_products_lambda_function)
 
+        http_api = apiGatewayV2.HttpApi(
             self,
             "Pantry Api",
             api_name="Pantry HTTP API",
@@ -43,8 +56,6 @@ class DevopsStack(Stack):
             default_authorizer=None,
         )
 
-        search_nutrition_products_lambda_integration = apiGatewayV2Integrations.HttpLambdaIntegration("LambdaIntegration",
-            handler=search_nutrition_products_lambda_function
         search_nutrition_products_lambda_integration = (
             apiGatewayV2Integrations.HttpLambdaIntegration(
                 "LambdaIntegration", handler=search_nutrition_products_lambda_function
